bfca879b132213c594f323c7b31c772ced3c07e3
Bob-the-Bobothy <126205683+Bob-the-Bobothy@users.noreply.github.com>
2026-09-07 20:52:36 -0700

    Fix activation script path for virtual environment

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 python3 -m venv .venv
 
-chmod +x ".venv/Scripts/activate"
-./".venv/Scripts/activate"
+chmod +x ".venv/bin/activate"
+./".venv/bin/activate"
 
 python3 -m pip install -r requirements.txt
 
