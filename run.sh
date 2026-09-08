9f3da48151f4407d57a48e38b4bacde8317f5772
Bob-the-Bobothy <126205683+Bob-the-Bobothy@users.noreply.github.com>
2026-09-07 21:02:21 -0700

    Refactor run.sh to remove venv setup and run scripts in background
    Removed virtual environment setup and pip install commands. Updated script to run main.py in the background for both dad-bot and vndb-quotes.

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-python3 -m venv .venv
-
 source ".venv/bin/activate"
 
-python3 -m pip install -r requirements.txt
-
 cd dad-bot
-python3 main.py
+python3 main.py & 
 
 cd ../vndb-quotes
-python3 main.py
+python3 main.py &
